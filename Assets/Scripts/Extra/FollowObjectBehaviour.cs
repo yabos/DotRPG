1b89ed153b9f4f7ef93deb380d28c351d7417357
yabos <yabos@naver.com>
2016-08-25 01:29:40 +0900

    telpo

---
+++ Assets/Scripts/Extra/FollowObjectBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CreativeSpore.RpgMapEditor
+{
+	public class FollowObjectBehaviour : MonoBehaviour {
+
+		public float DampTime = 0.15f;
+		public Transform Target;
+
+		private Vector3 velocity = Vector3.zero;
+        private Camera m_camera;
+
+        private bool m_bNotSmooth = false;
+        public bool NotSmooth
+        {
+            set { m_bNotSmooth = value; }
+        }
+
+        void Start()
+        {
+            m_camera = GetComponent<Camera>();
+        }
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			if (Target)
+			{
+                if (!m_bNotSmooth)
+                {
+                    Vector3 point = m_camera.WorldToViewportPoint(Target.position);
+                    Vector3 delta = Target.position - m_camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+                    Vector3 destination = transform.position + delta;
+                    transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, DampTime);
+                }
+                else
+                {
+                    Vector3 point = m_camera.WorldToViewportPoint(Target.position);
+                    Vector3 delta = Target.position - m_camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+                    Vector3 destination = transform.position + delta;
+                    transform.position = destination;
+                    m_bNotSmooth = false;
+                }
+			}		
+		}
+	}
+}
